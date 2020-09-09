40843bae9c108771c59de08c1ca1f085596ddc99
PiotrGrzyb <piotrg6711@gmail.com>
2020-09-09 13:42:47 +0200

    Nothing new

--- Test.py
+++ Test.py
@@ -95,6 +95,8 @@ def main():
     run = True
     while run:
         clock.tick(30)
+        mouse = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
